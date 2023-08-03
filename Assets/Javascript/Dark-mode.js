d8240bdecaf14756c0bc5a6da3af725f92db7465
Leozinho2831 <leoferraz1818@gmail.com>
2023-08-02 21:46:14 -0300

    Commit: corrected code in dark to light

--- Assets/Javascript/Dark-mode.js
+++ Assets/Javascript/Dark-mode.js
@@ -1,14 +1,14 @@
-function darkMode(){
+function lightMode(){
     const body = document.querySelector('body');
-    if (body.classList.contains('dark')){
-        body.classList.remove('dark');       
+    if (body.classList.contains('light')){
+        body.classList.remove('light');       
     } else {
-        body.classList.add('dark');
+        body.classList.add('light');
     }
 }
 
 const modeCheckbox = document.getElementById('mode');
-modeCheckbox.addEventListener('click', darkMode);
+modeCheckbox.addEventListener('click', lightMode);
 
 const modeCheckboxMobile = document.getElementById('mode-mobile');
-        modeCheckboxMobile.addEventListener('click', darkMode);
+        modeCheckboxMobile.addEventListener('click', lightMode);
